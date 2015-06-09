c2cf8c575d5302a73c3c47b90235050fe969d9df
unknown <espnosa@ESPNOSA1.americas.hpqcorp.net>
2015-06-09 14:19:45 -0500

    running demo

--- MathServiceDemo/src/client.java
+++ MathServiceDemo/src/client.java
@@ -1,9 +1,14 @@
 import java.rmi.Naming;
 
-
+/**
+ * Client must contact an RMI registry, and request the name of the service. <br/>
+ * Client won't know the exact location this service, but he knows enough to contact it's registry. <br/>
+ * The service may change regularly, so client doesn't have a copy of the class (MathService).
+ * @author espnosa
+ */
 public class client {
 	public static void main ( String args[] ) throws Exception {		
-		String url = "rmi://localhost:6789/MathService";
+		String url = "rmi://localhost:1099/MathService";
 		MathService mathService = (MathService)Naming.lookup(url);
 		
 		int square = mathService.square(2);
--- MathServiceDemo/src/server.java
+++ MathServiceDemo/src/server.java
@@ -1,12 +1,18 @@
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
+/**
+ * Code responsible for creating an instance of MathService <br/>
+ * and registering (or binding) the service with the RMI Registry <br/>
+ * Registry port is 1099.
+ * @see <li>http://stackoverflow.com/questions/23104661/rmi-connection-refused-on-localhost
+ * @see <li>http://www.javacoffeebreak.com/articles/javarmi/javarmi.html
+ */
 public class server {
 	public static void main ( String args[] ) throws Exception {
-	   // Create an instance of our service
-	   System.setProperty("java.rmi.server.hostname", "127.0.0.2");
-	   MathServiceImpl impl = new MathServiceImpl();
-	   Naming.rebind ("MathService", impl);
-	   System.out.println ("Service bound.");
+		LocateRegistry.createRegistry(1099);
+		MathServiceImpl impl=new MathServiceImpl();
+		Naming.rebind("//localhost:1099/MathService", impl);
+		System.out.println("Implementation has been bind and is ready for use");
 	}
-	
 }
